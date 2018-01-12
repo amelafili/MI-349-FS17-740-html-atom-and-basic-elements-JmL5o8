9554a692590d14af91d7107dfac10e1c43bd8596
amelafili <31477792+amelafili@users.noreply.github.com>
2018-01-12 12:04:40 -0500

    fixed chart errors

---
+++ proj01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 11:28:59 2018
+
+@author: amelafili
+"""
+
+# first program in python
+# input two numbers, add them together, print them out
+# wfp, 9/1/07; rje, 5/5/14
+
+num_str1 = input('Input rods: ')
+int1 = (num_str1)
+print('You input',int1,'rods ')
+print('Conversions')
+print('Meters: ',int1*5.029)
+print('Feet: ',int1*16.5)
+print('Miles: ',int1*.003)
+print('Furlongs: ',int1*0.025)
+print('Minutes to walk 1.0 rods: ',int1*0.06)
